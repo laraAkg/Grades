f4f5c5c05812aa141effd5eca2e24e32a089839f
zakgul-bbc <Lara.Akguen@bbcag.ch>
2019-07-03 15:45:56 +0200

    6 commit

--- OneDrive/Documents/GitHub/Grades/app/src/main/java/com/example/laraa/grades/MainActivity.java
+++ OneDrive/Documents/GitHub/Grades/app/src/main/java/com/example/laraa/grades/MainActivity.java
@@ -12,7 +12,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
